6cf0b72b4a5420a95cab7d7c47b9b8eb7428681a
Eric Johnson <eric.johnson@mycase.com>
2015-11-12 13:23:07 -0800

    emj - book club meeting

--- src/clojure_life/core.clj
+++ src/clojure_life/core.clj
@@ -3,6 +3,33 @@
 (defn -main [& args]
       (println "foobar"))
 
+(def blinker-board `((0 0 0 0 0)(0 0 0 0 0)(0 1 1 1 0)(0 0 0 0 0)(0 0 0 0 0)))
+
+(defn print-board [board]
+      (do
+	(map println blinker-board)))
+
+(defn sum-neighbors [x y board]
+      
+      ))
+
+(defn cell-val [x y board]
+      (let [x-size (count board)
+      	    y-size (count (nth board 0))]
+	    (if (or (x > x-size)
+	    	    (y > y-size)
+		    (x < 0)
+		    (y < 0))
+		    0)
+		    (nth (nth board y) x)
+
+(defn adjacency [x y board]
+      	    (+ (cell-val [(- x 1) y board])
+	       (cell-val [(+ x 1) y board])
+	       (cell-val [x (- y 1) board])
+	       (cell-val [x (+ y 1) board]))
+	    
+
 (defn foo
   "I don't do a whole lot."
   [x]
